7d24598ed2e915e89862bbe141f4051f8343b5eb
wnals9 <wnals16@naver.com>
2023-07-17 10:42:25 +0900

    20230717

--- 04_Control_Ex/src/ex02_loop/MainWrapper.java
+++ 04_Control_Ex/src/ex02_loop/MainWrapper.java
@@ -8,8 +8,8 @@ public class MainWrapper {
     // ...
     // 2 x 9 = 18
     int dan = 2;  // 이 곳에 원하는 구구단을 넣으면 된다.
-    for(int i= 1; i<10; i++) {
-      System.out.println(dan + " x " + i + " = " + dan * i);
+    for(int i= 1; i<=9; i++) {
+      System.out.println(dan + " x " + i + " = " + dan*i);
     }
   }
   
@@ -20,14 +20,15 @@ public class MainWrapper {
     // 3회 모금액 60원, 현재 모금액 180원
     // ...
     // 1667회 모금액 60원, 현재 모금액 100020원
-    int goal = 100000;  // 목표 모금액
+    final int GOAL = 100000;  // 목표 모금액
     int money = 60;  // 1회당 모금액
     int total = 0;  // 모금액 합계
     int nth = 0;  // 회차
-    while(total < goal) {
-      total = nth*money;
-      System.out.println(nth + "회" + " 모금액 " + money + "원," + " 현재 모금액 " + total);
+    while(total <= GOAL) {
+      total += money;
       nth++;
+      System.out.println(nth + "회 모금액 " + money + "원, 현재 모금액 " + total);
+      
     }
     
     
@@ -38,9 +39,10 @@ public class MainWrapper {
     // 2 x 1 = 2
     // ...
     // 9 x 9 = 81
-    for(int i= 2; i < 10; i++) {
-      for(int j= 1; j < 10; j++) {
-   System.out.println(i+"x"+j+"="+i*j);     
+    // 고정 값 : 바깥 LOOP에 배치
+    for(int dan= 2; dan <= 9; dan++) {
+      for(int j= 1; j <= 9; j++) {
+   System.out.println(dan+"x"+j+"="+dan*j);     
   }
     }}
   public static void ex04() {
@@ -52,81 +54,112 @@ public class MainWrapper {
     // 참고할코드
     //System.out.print("2x1=2");  // 2x1=2를 출력한 뒤 줄을 바꾸지 않는다.
     
-    for(int i= 1; i < 10; i++) {
-     for(int j= 2; j < 10; j++){
-     System.out.print(j+"x"+i+"="+i*j+" ");
-     }System.out.println("");
-  }
-  }
+    for(int i= 1; i <= 9; i++) {
+     for(int dan= 2; dan <= 9; dan++){
+       System.out.print(dan+"x"+i+"=" + String.format("%-4d", dan*i));
+     }
+     System.out.println();  // 줄 바꿈
+   }
+ }
   public static void ex05() {
     // 삼각별 출력하기 - 1 (2중 loop)
-    //     b=12345
-    // a=1 *
-    // a=2 **
-    // a=3 ***
-    // a=4 ****
-    // a=5 *****
+    //      star=12345
+    // row=1     *
+    // row=2     **
+    // row=3     ***
+    // row=4     ****
+    // row=5     *****
+    //
+    // row=1, star=1~1
+    // row=2, star=1~2
+    // row=3, star=1~3
+    // row=4, star=1~4
+    // row=5, star=1~5    
     
-    // 참고할 코드
-    //System.out.print("*");  // *를 출력한 뒤 줄을 바꾸지 않는다.
-    //System.out.println();   // 줄 바꾸기
-    
-    for(int a= 1; a < 6; a++) {
-      for(int b= 0; b < a; b++) {
+    for(int row= 1; row <= 6; row++) {
+      for(int star= 1; star <= row; star++) {
         System.out.print("*");
-      }System.out.println("");
       }
+      System.out.println();
     }
+  }
   
   public static void ex06() {
     // 삼각별 출력하기 - 2 (2중 loop)
-    //     b=12345
-    // a=1 *****
-    // a=2 ****
-    // a=3 ***
-    // a=4 **
-    // a=5 *
-    for(int a = 1; a<6; a++) {
-      for(int b = 0; b < 6-a; b++) {
+    //      star=12345
+    // row=1     *****
+    // row=2     ****
+    // row=3     ***
+    // row=4     **
+    // row=5     *
+    //
+    // row=1, star=1~5
+    // row=2, star=1~4
+    // row=3, star=1~3
+    // row=4, star=1~2
+    // row=5, star=1~1
+    for(int row = 1; row <= 5; row++) {
+      for(int star = 1; star <= 6-row; star++) {
         System.out.print("*");
-      }System.out.println("");
+      }
+      System.out.println();
     }
   }
   
   public static void ex07() {
     // 삼각별 출력하기 - 3 (2중 loop)
     //
-    //    b=123456789
-    //a=1     *
-    //a=2    ***
-    //a=3   *****
-    //a=4  *******
-    //a=5 *********
-    for(int a= 1; a < 6; a++) {
-      for(int b= 1; b < 6-a; b++) {
-        System.out.print("x");
+    //     space/star=123456789
+    //row=1               *
+    //row=2              ***
+    //row=3             *****
+    //row=4            *******
+    //row=5           *********
+    //
+    //row=1, space=1~4, star=5~5
+    //row=2, space=1~3, star=4~6
+    //row=3, space=1~2, star=3~7
+    //row=4, space=1~1, star=2~8
+    //row=5, space=1~0, star=1~9
+    for(int row= 1; row <= 5; row++) {
+      for(int space= 1; space <= 5-row; space++) {
+        System.out.print(" ");
       }
-      for(int b= 1; b < a*2; b++) {
+      for(int star= 6-row; star <= row+4; star++) {
         System.out.print("*");
       }
       
-      System.out.println("x");
+      System.out.println();
     }
   }
   
   public static void ex08() {
     // 삼각별 출력하기 - 4 (2중 loop)
-    //    b=123456789
-    //a=1 *********
-    //a=2  *******
-    //a=3   *****
-    //a=4    ***
-    //a=5     *
+    //    space/star=123456789
+    //row=1          *********
+    //row=2           *******
+    //row=3            *****
+    //row=4             ***
+    //row=5              *
+    //
+    //row=1, space=1~0, star=1~9
+    //row=2, space=1~1, star=2~8
+    //row=3, space=1~2, star=3~7
+    //row=4, space=1~3, star=4~6
+    //row=5, space=1~4, star=5~5
+    for(int row=1; row <= 5; row++) {
+      for(int space=1; space <= row-1; space++) {
+        System.out.print(" ");
+      }
+     for(int star=row; star <= 10-row; star++) {
+      System.out.print("*");
+    }
+    System.out.println();
   }
-  
+ }
   
   public static void main(String[] args) {
-ex08();
+    ex08();
   }
 
 }
