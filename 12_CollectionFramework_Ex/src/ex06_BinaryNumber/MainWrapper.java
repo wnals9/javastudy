c4c8616121e3d919e9c58818f51c6c322c682b44
wnals9 <wnals16@naver.com>
2023-07-28 15:20:01 +0900

    20230728

---
+++ 12_CollectionFramework_Ex/src/ex06_BinaryNumber/MainWrapper.java
@@ -0,0 +1,12 @@
+package ex06_BinaryNumber;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    BinaryNumber bn = new BinaryNumber(0);
+    System.out.println(bn);  // 100000
+    
+  }
+
+}
