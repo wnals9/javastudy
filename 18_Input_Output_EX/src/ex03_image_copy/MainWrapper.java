39f399b3c245bf7b1f7fa2b7dc18285b17c57d9c
wnals9 <wnals16@naver.com>
2023-08-04 15:22:02 +0900

    20230804

---
+++ 18_Input_Output_EX/src/ex03_image_copy/MainWrapper.java
@@ -0,0 +1,12 @@
+package ex03_image_copy;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // 문제. %ORA_HOME%/oui/instImages/bg.jpg 이미지 파일을 C:/storage/bg.jpg 이미지로 복사하시오.
+    // %ORA_HOME%은 C:/app/사용자/product/18.0.0/dbhomeXE 디렉터리를 의미한다.
+    
+  }
+
+}
