f79dcb5802fa962be067e1e0f44db2c42bc6293f
wnals9 <wnals16@naver.com>
2023-08-08 14:29:04 +0900

    20230808

--- 19_Network/src/ex02_api/NaverCaptcha.java
+++ 19_Network/src/ex02_api/NaverCaptcha.java
@@ -6,8 +6,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 import org.json.JSONObject;
 
@@ -68,16 +70,17 @@ public class NaverCaptcha {
     
 }
   
-  private static void getImage() {
+  private static String getImage() {
     
     URL url = null;
     HttpURLConnection con = null;
     BufferedInputStream bin = null;
     BufferedOutputStream bout = null;
+    String key = null;
     
     try {
       
-      String key = getKey();
+      key = getKey();
       String spec = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key;
       
       url = new URL(spec);
@@ -121,10 +124,70 @@ public class NaverCaptcha {
       e.printStackTrace();
     }
   }
-    
+    return key;
 }  
+  
+  private static void validInput() {
+    
+    URL url = null;
+    HttpURLConnection con = null;
+    BufferedReader reader = null;
+    
+    try {
+      
+      String key = getImage();
+      
+      Scanner sc = new Scanner(System.in);
+      System.out.println("입력 >> ");
+      String value = sc.next();
+      
+      String spec = "https://openapi.naver.com/v1/captcha/nkey?code=1&key=" + key + "&value=" + value;
+      
+      url = new URL(spec);
+      con = (HttpURLConnection) url.openConnection();
+      
+      con.setRequestMethod("GET");
+      
+      con.setRequestProperty("X-Naver-Client-Id", CLIENT_ID);
+      con.setRequestProperty("X-Naver-Client-Secret", CLIENT_SECRET);
+      
+      int responseCode = con.getResponseCode();
+      if(responseCode != HttpURLConnection.HTTP_OK) {
+        throw new RuntimeException(responseCode + " 발생");
+      }
+      
+      reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      
+      StringBuilder sb = new StringBuilder();
+      String line = null;
+      while((line = reader.readLine()) != null) {
+        sb.append(line);
+      }
+      
+      JSONObject obj = new JSONObject(sb.toString());
+      System.out.println("응답시간: " + obj.getDouble("responseTime"));
+      if(obj.getBoolean("result")) {
+        System.out.println("맞습니다.");
+      } else {
+        System.out.println("틀립니다");
+      }
+      
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    } finally {
+      try {
+        if(reader != null) reader.close();
+        if(con != null) con.disconnect();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    
+    
+  }
+  
   public static void main(String[] args) {
-    getImage();
+    validInput();
   }
 
 }
